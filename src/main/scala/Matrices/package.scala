d88c5cc9eb07222dc24d68df114ce113b963e7ec
AngieJoya99 <angie.joya@correounivalle.edu.co>
2024-04-27 12:06:10 -0500

    Crear proyecto

---
+++ src/main/scala/Matrices/package.scala
@@ -0,0 +1,163 @@
+/* Angie Joya - 2322609
+Emily Nuñez - 2240156*/
+
+import common._
+import scala.util.Random
+import scala.collection.parallel.immutable.ParVector
+package object Matrices {
+    val random = new Random()
+    type Matriz = Vector[Vector[Int]]
+
+    /**
+      * Crea una matriz cuadrada de dimensiones long x long
+      * con valores aleatorios entre 0 y vals
+      * @param long dimensiones de la matriz
+      * @param vals valor máximo del aleatorio
+      * @return matriz cuadrada
+      */
+    def matrizAlAzar (long:Int, vals:Int): Matriz ={
+        val v = Vector.fill(long,long){random.nextInt(vals)}
+        v
+    }
+
+    /**
+      * Traspone una matriz m
+      * @param m matriz
+      * @return matriz transpuesta
+      */
+    def transpuesta(m:Matriz): Matriz ={
+        val l = m.length
+        Vector.tabulate(1,1)((i,j) => m(j)(i))
+    }
+
+    /**
+      * Calcula el producto punto entre dos vectores v1 y v2
+      * @param v1 vector 1
+      * @param v2 vector 1
+      * @return producto punto entre v1 y v2
+      */
+    def prodPunto(v1:Vector[Int], v2:Vector[Int]): Int ={
+        (v1 zip v2).map({case (i,j)=> (i*j)}).sum
+    }
+
+    /**
+      * 
+      *
+      * @param v1
+      * @param v2
+      * @return
+      */
+    def prodPuntoParD(v1:ParVector[Int], v2:ParVector[Int]): Int ={
+        (v1 zip v2).map({case (i,j)=> (i*j)}).sum
+    }
+
+    /**
+      * Ejercicio 1.1.1
+      *
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return
+      */
+    def multMatriz(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.1.2
+      *
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return
+      */
+    def multMatrizPar(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.2.1
+      * Dada una matriz cuadrada m de tamaño NxN, 1<=i, j<=N, i+n<=N, j+n<=N,
+      * Retorna la submatriz de NxN correspondiente a m[i..i+(n-1) , j..j+(n-1)]
+      * @param m Matriz
+      * @param i
+      * @param j
+      * @param l
+      * @return Submatriz
+      */
+    def subMatriz(m:Matriz, i:Int, j:Int, l:Int): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.2.2
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la suma de las dos matrices
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Suma de m1 y m2
+      */
+    def sumMatriz(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.2.3
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la multiplicación de las dos matrices
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Multiplicación de m1 y m2
+      */
+    def multMatrizRec(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.2.4
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la multiplicación de las dos matrices en paralelo
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Multiplicación de m1 y m2
+      */
+    def multMatrizRecPar(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.3.1
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la resta de las dos matrices
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Resta de m1 y m2
+      */
+    def restaMatriz(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.3.2
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la multiplicación de las dos matrices usando el algortimo de Strassen
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Multiplicación de m1 y m2
+      */
+    def multStrassen(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }
+
+    /**
+      * Ejercicio 1.3.3
+      * Dadas dos matrices cuadradas del mismo tamaño (potencia de 2) m1 y m2,
+      * calcula la multiplicación en paralelo de las dos matrices usando el 
+      * algortimo de Strassen
+      * @param m1 Matriz 1
+      * @param m2 Matriz 2
+      * @return Multiplicación de m1 y m2
+      */
+    def multStrassenPar(m1:Matriz, m2:Matriz): Matriz ={
+        Vector(Vector(0))
+    }  
+}
+
